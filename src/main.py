d9cb0d0cdd3b30c86ddef52f8cff4b7e4ffdc231
Abdulla Kurmanbaev <abdullakurmanbaev38@gmail.com>
2025-02-01 23:10:03 +0600

    refactor: improve code structure and readability

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import pvporcupine
 from pvrecorder import PvRecorder
+from vosk import Model, KaldiRecognizer
+
 import pyaudio
 import wave
+
+import json
+import array
 
 from os import getenv
 from dotenv import load_dotenv, find_dotenv
@@ -9,6 +14,9 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 access_key = getenv("PICOVOICE")
+model = Model("./src/model")
+recognizer = KaldiRecognizer(model, 16000)
+CHUNK = 1024
 
 if not access_key:
     raise ValueError("""Access_key is missing! 
@@ -22,37 +30,48 @@ porcupine = pvporcupine.create(
 recorder = PvRecorder(device_index=5, frame_length=porcupine.frame_length)
 recorder.start()
 print("Using device: %s" % recorder.selected_device)
-try:
-    while True:
-        pcm = recorder.read()
-        keyword_index = porcupine.process(pcm)
-        if keyword_index == 0:
-            print("Hi.")
-            with wave.open("./src/sounds/VoiceMor.wav", "rb") as wf:
-                # Instantiate PyAudio and initialize PortAudio system resources (1)
-                p = pyaudio.PyAudio()
 
-                # Open stream (2)
-                stream = p.open(
-                    format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True,
-                )
 
-                # Play samples from the wave file (3)
-                while len(data := wf.readframes(1024)):  # Requires Python 3.8+ for :=
-                    stream.write(data)
+def play_sound():
+    with wave.open("./src/sounds/VoiceMor.wav", "rb") as wf:
+        # Instantiate PyAudio and initialize PortAudio system resources (1)
+        p = pyaudio.PyAudio()
+        # Open stream (2)
+        stream = p.open(
+            format=p.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True,
+        )
+        # Play samples from the wave file (3)
+        while len(data := wf.readframes(CHUNK)):  # Requires Python 3.8+ for :=
+            stream.write(data)
+            # Close stream (4)
+            stream.close()
+            # Release PortAudio system resources (5)
+            p.terminate()
 
-                # Close stream (4)
-                stream.close()
 
-                # Release PortAudio system resources (5)
-                p.terminate()
+def main():
+    try:
+        while True:
+            pcm = recorder.read()
+            keyword_index = porcupine.process(pcm)
+            if keyword_index == 0:
+                print("Hi.")
+                play_sound()
+            pcm_bytes = array.array("h", pcm).tobytes()
+            if recognizer.AcceptWaveform(pcm_bytes):
+                result = json.loads(recognizer.Result())
+                print(result.get("text", ""))
 
-except KeyboardInterrupt:
-    print("\nStopping...")
-finally:
-    recorder.stop()
-    recorder.delete()
-    porcupine.delete()
+    except KeyboardInterrupt:
+        print("\nStopping...")
+    finally:
+        recorder.stop()
+        recorder.delete()
+        porcupine.delete()
+
+
+if __name__ == "__main__":
+    main()
