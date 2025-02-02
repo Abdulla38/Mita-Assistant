08a2a9964c5ce208b05ba9af301deec05be9ecbe
Abdulla <abdullakurmanbaev38@gmail.com>
2025-02-02 20:12:45 +0600

    refactor: add type annotation and default value

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,11 +1,12 @@
+from typing import Optional
 from os import getenv
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-ACCESS_KEY = getenv("PICOVOICE")
-CHAT_GPT = getenv("GPT_API_KEY")
-ELEVENLABS = getenv("ELEVENLABS")
+ACCESS_KEY: Optional[str] = getenv("PICOVOICE")
+CHAT_GPT: Optional[str] = getenv("GPT_API_KEY")
+ELEVENLABS: Optional[str] = getenv("ELEVENLABS")
 
 if not ACCESS_KEY:
     raise ValueError("""Access_key is missing! 
