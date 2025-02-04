1a7fd19390857def4d655e7fef2f95a91ba0978e
Abdulla <abdullakurmanbaev38@gmail.com>
2025-02-04 19:37:26 +0600

    refactor: import modules and start in enter point

--- src/main.py
+++ src/main.py
@@ -1,60 +1,18 @@
-import pvporcupine
-from pvrecorder import PvRecorder
-
-import pyaudio
-import wave
-
-from config import ACCESS_KEY
-
-CHUNK = 1024
-
-porcupine = pvporcupine.create(
-    access_key=ACCESS_KEY,
-    keyword_paths=["./src/wake word activation/Hey-Mita_en_linux_v3_0_0.ppn"],
-)
-
-recorder = PvRecorder(device_index=5, frame_length=porcupine.frame_length)
-recorder.start()
-print("Using device: %s" % recorder.selected_device)
-
-
-def play_sound():
-    with wave.open("./src/assets/sounds/VoiceMor.wav", "rb") as wf:
-        # Instantiate PyAudio and initialize PortAudio system resources (1)
-        p = pyaudio.PyAudio()
-        # Open stream (2)
-        stream = p.open(
-            format=p.get_format_from_width(wf.getsampwidth()),
-            channels=wf.getnchannels(),
-            rate=wf.getframerate(),
-            output=True,
-        )
-        # Play samples from the wave file (3)
-        data = wf.readframes(CHUNK)
-        while data:  # Requires Python 3.8+ for :=
-            stream.write(data)
-            data = wf.readframes(CHUNK)
-        # Close stream (4)
-        stream.close()
-        # Release PortAudio system resources (5)
-        p.terminate()
+from .detection import WakeWordDetector
+from .player import play_sound
 
 
 def main():
+    detector = WakeWordDetector(keyword_path="./wake/Hey-Mita_en_linux_v3_0_0.ppn")
+    detector.start()
     try:
         while True:
-            pcm = recorder.read()
-            keyword_index = porcupine.process(pcm)
-            if keyword_index == 0:
-                print("Hi.")
+            if detector.detect():
                 play_sound()
-
     except KeyboardInterrupt:
         print("\nStopping...")
     finally:
-        recorder.stop()
-        recorder.delete()
-        porcupine.delete()
+        detector.stop()
 
 
 if __name__ == "__main__":
